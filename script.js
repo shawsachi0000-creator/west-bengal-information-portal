2368e7ea3d2b6c32109526ab5aae87f5d38e22e6
shawsachi0000-creator <shawsachi0000@gmail.com>
2026-07-18 08:58:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -227,3 +227,44 @@ copy.innerHTML=`© ${new Date().getFullYear()} West Bengal Information Portal. A
 }
 
 console.log("West Bengal Information Portal Loaded Successfully.");
+// Part 1
+
+...
+
+// Part 2
+
+...
+
+console.log("West Bengal Information Portal Loaded Successfully.");
+
+// ===============================
+// IMAGE SLIDER
+// ===============================
+
+let slideIndex = 0;
+
+showSlides();
+
+function showSlides() {
+
+let slides = document.getElementsByClassName("slides");
+
+for (let i = 0; i < slides.length; i++) {
+
+slides[i].style.display = "none";
+
+}
+
+slideIndex++;
+
+if (slideIndex > slides.length) {
+
+slideIndex = 1;
+
+}
+
+slides[slideIndex - 1].style.display = "block";
+
+setTimeout(showSlides, 3000);
+
+}
